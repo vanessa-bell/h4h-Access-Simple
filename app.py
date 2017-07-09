389a60f1aca3d9da0c6c4f293806e19e82138c9c
Harmann Bajwa <harmannbajwa@Harmanns-MacBook-Pro.local>
2017-07-09 12:02:04 -0700

    updating queries

--- app.py
+++ app.py
@@ -56,56 +56,115 @@ def login_required(test):
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
-key = API_KEY
-google_places = GooglePlaces(key)
-query_result = google_places.nearby_search(
-        location='San Francisco, CA',
-        radius=50000, types=[types.TYPE_PHARMACY])
+def google_search(location):
+    key = API_KEY
+    google_places = GooglePlaces(key)
+    query_result = google_places.nearby_search(
+            location=location,
+            radius=50000, types=[types.TYPE_PHARMACY])
 
-# pdb.set_trace()
-
-
-if query_result.has_attributions:
-    print query_result.html_attributions
-
-for place in query_result.places:
-  # print place.name
-
-  # The following method has to make a further API call.
-  place.get_details()
-  # Referencing any of the attributes below, prior to making a call to
-  # get_details() will raise a googleplaces.GooglePlacesAttributeError.
-  # pp = pprint.PrettyPrinter(indent=4)
-  # pp.pprint(place.details) # A dict matching the JSON response from Google.
-
-  # print place.place_id
-  # print place.name
-  # print place.formatted_address
-  # print place.local_phone_number
-  place_id = place.place_id
-  name = place.name
-  address =  place.formatted_address
-  phone_number = place.local_phone_number
-  website = place.website
+    # pdb.set_trace()
 
 
-  add_pharmacy = """INSERT OR IGNORE INTO PHARMACIES (PLACE_ID, NAME, ADDRESS, PHONE_NUMBER, WEBSITE, PRESCRIBES)
-    VALUES (?, ?, ?, ?, ?, ?);"""
-  pharmacy_data = (place_id, name, address, phone_number, website, 'NO')
+    if query_result.has_attributions:
+        print query_result.html_attributions
 
-  conn.execute(add_pharmacy,pharmacy_data)
+    places_list = []
+    places_dicts = {}
+    display_dict = {}
 
-pharmacy_IDs_query = """
-SELECT
-    PLACE_ID
-FROM
-    PHARMACIES"""
+    count = 0
+
+    for place in query_result.places:
+      count += 1
+
+      place_dict = {}
+      # print place.name
+
+      # The following method has to make a further API call.
+      place.get_details()
+      # Referencing any of the attributes below, prior to making a call to
+      # get_details() will raise a googleplaces.GooglePlacesAttributeError.
+      # pp = pprint.PrettyPrinter(indent=4)
+      # pp.pprint(place.details) # A dict matching the JSON response from Google.
+
+      # print place.place_id
+      # print place.name
+      # print place.formatted_address
+      # print place.local_phone_number
+      place_id = place.place_id
+      name = place.name
+      address =  place.formatted_address
+      phone_number = place.local_phone_number
+      website = place.website
+
+      place_dict['name'] = name
+      place_dict['address'] = address
+      place_dict['phone_number'] = phone_number
+      place_dict['website'] = website
+
+      place_dict['place_id'] = place_id
+
+      places_dicts[place_id] = place_dict
+
+      places_list.append(place_id)
 
 
-c = conn.cursor()
-c.execute(pharmacy_IDs_query)
-IDS = c.fetchall()
-print(IDS)
+
+      add_pharmacy = """INSERT OR IGNORE INTO PHARMACIES (PLACE_ID, NAME, ADDRESS, PHONE_NUMBER, WEBSITE, PRESCRIBES)
+        VALUES (?, ?, ?, ?, ?, ?);"""
+      pharmacy_data = (place_id, name, address, phone_number, website, 'UNK')
+
+      conn.execute(add_pharmacy,pharmacy_data)
+
+
+    c = conn.cursor()
+
+    update_query = """
+    UPDATE
+      PHARMACIES
+    SET
+      PRESCRIBES = 'YES'
+    WHERE
+      PLACE_ID = 'ChIJUfiUFleHhYARA0hMzaudAmM'"""
+
+    c.execute(update_query)
+
+    places_dict = {}
+
+    for place in places_list:
+      pharmacy_IDs_query = """
+      SELECT
+          PRESCRIBES
+      FROM
+          PHARMACIES
+      WHERE
+          PLACE_ID = ?"""
+
+      c.execute(pharmacy_IDs_query,(place,))
+      prescribe_bool = c.fetchall()
+
+      places_dict[place] = prescribe_bool
+      places_dicts[place]['prescribes'] = prescribe_bool
+
+  # Filter out just the three closest results
+
+  # test
+    for p in places_dicts.keys():
+        if places_dicts[p]['prescribes'] == 'YES':
+            print(p)
+            print(places_dicts[p])
+    # print(places_dicts)
+    return places_dicts
+
+location = '350 Parnassus Ave, San Francisco'
+
+google_search(location)
+
+
+# c.execute(pharmacy_IDs_query)
+# IDS = c.fetchall()
+# print(IDS)
 
 
 conn.commit()
